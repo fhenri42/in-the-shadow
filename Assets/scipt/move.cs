b53fbe2257d1fdfe2a74fa014e106ef831f49969
Henri Floren <fhenri@student.42.fr>
2018-09-25 17:28:47 +0200

    [ADD] init project need roation and menu and will be sonde

---
+++ Assets/scipt/move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+public float speed = 0.1f;
+public GameObject test;
+
+     public float horizontalSpeed = 2.0F;
+     public float verticalSpeed = 2.0F;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		print(Input.mousePosition);
+
+		float h = horizontalSpeed * Input.GetAxis("Mouse X");
+		 float v = verticalSpeed * Input.GetAxis("Mouse Y");
+		 test.transform.Rotate(v, h, 0);
+		// test.transform.Rotate( Input.mousePosition* Time.deltaTime * speed);
+	}
+}
